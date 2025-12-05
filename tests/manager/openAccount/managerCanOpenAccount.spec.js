07e199ce6492d8ec0c977c2c173ea3a9c5a30c70
JoAnna578 <joannakoloczek1@gmail.com>
2025-12-05 18:13:28 +0100

    Update managerCanOpenAccount.spec.js
    Add test for creating a new account for customer using POM.

--- tests/manager/openAccount/managerCanOpenAccount.spec.js
+++ tests/manager/openAccount/managerCanOpenAccount.spec.js
@@ -1,31 +1,35 @@
 import { test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
+import { ManagerAddCustomerPage } from '../../../src/pages/manager/ManagerAddCustomerPage';
+import { ManagerOpenAccountPage } from '../../../src/pages/manager/ManagerOpenAccountPage';
+import { ManagerCustomerListPage } from '../../../src/pages/manager/ManagerCustomerListPage';
+
+let firstName, lastName, postCode;
 
 test.beforeEach(async ({ page }) => {
-  /* 
-  Pre-conditons:
-  1. Open Add Customer page
-  2. Fill the First Name.  
-  3. Fill the Last Name.
-  4. Fill the Postal Code.
-  5. Click [Add Customer].
-  6. Reload the page (This is a simplified step to close the popup).
-  */
+  firstName = faker.person.firstName();
+  lastName = faker.person.lastName();
+  postCode = faker.location.zipCode();
+
+  const addCustomerPage = new ManagerAddCustomerPage(page);
+
+  await addCustomerPage.open();
+  await addCustomerPage.fillFirstName(firstName);
+  await addCustomerPage.fillLastName(lastName);
+  await addCustomerPage.fillPostCode(postCode);
+  await addCustomerPage.clickAddCustomer();
+  await addCustomerPage.reloadPage();
 });
 
 test('Assert manager can add new customer', async ({ page }) => {
-  /* 
-  Test:
-  1. Click [Open Account].
-  2. Select Customer name you just created.
-  3. Select currency.
-  4. Click [Process].
-  5. Reload the page (This is a simplified step to close the popup).
-  6. Click [Customers].
-  7. Assert the customer row has the account number not empty.
+  const openAccountPage = new ManagerOpenAccountPage(page);
+  const customerListPage = new ManagerCustomerListPage(page);
 
-  Tips:
-  1. Do not rely on the customer row id for the step 13. 
-    Use the ".last()" locator to get the last row.
-  */
+  await openAccountPage.open();
+  await openAccountPage.selectCustomer(`${firstName} ${lastName}`);
+  await openAccountPage.selectCurrency('Dollar');
+  await openAccountPage.clickProcess();
+  await openAccountPage.reloadPage();
+  await customerListPage.open();
+  await customerListPage.assertLastRowHasAccountNumber();
 });
