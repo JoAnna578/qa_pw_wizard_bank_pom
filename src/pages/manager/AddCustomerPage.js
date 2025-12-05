5b4a0053d693d0d055e8a2de279674017fec50a8
JoAnna578 <joannakoloczek1@gmail.com>
2025-12-05 19:02:24 +0100

    Update AddCustomerPage.js
    Fix syntax and complete Manager AddCustomerPage POM class

--- src/pages/manager/AddCustomerPage.js
+++ src/pages/manager/AddCustomerPage.js
@@ -3,6 +3,11 @@ import { expect } from '@playwright/test';
 export class AddCustomerPage {
   constructor(page) {
     this.page = page;
+    this.firstNameInput = page.locator('input[placeholder="First Name"]');
+    this.lastNameInput = page.locator('input[placeholder="Last Name"]');
+    this.postCodeInput = page.locator('input[placeholder="Post Code"]');
+    this.addButton = page.locator('button[type="submit"]'); 
+    this.alert = page.locator('.alert');
   }
 
   async open() {
@@ -10,4 +15,28 @@ export class AddCustomerPage {
       '/angularJs-protractor/BankingProject/#/manager/addCust',
     );
   }
+   async fillFirstName(name) {
+    await this.firstNameInput.fill(name);
+  }
+
+  async fillLastName(name) {
+    await this.lastNameInput.fill(name);
+  }
+
+  async fillPostCode(code) {
+    await this.postCodeInput.fill(code);
+  }
+
+  async clickAddCustomer() {
+    await this.addButton.click();
+  }
+
+  async assertCustomerAddedAlertVisible() {
+    await expect(this.alert).toBeVisible();
+  }
+  
+  async reloadPage() {
+    await this.page.reload();
+  }
 }
+
