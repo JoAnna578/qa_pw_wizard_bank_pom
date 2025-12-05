da047760e01e9fa34fa3998c44f393d3bbbae4a0
JoAnna578 <joannakoloczek1@gmail.com>
2025-12-05 17:36:06 +0100

    Update managerCanLogin.spec.js
    Add manager login test using POM

--- tests/manager/bankManagerLogin/managerCanLogin.spec.js
+++ tests/manager/bankManagerLogin/managerCanLogin.spec.js
@@ -1,13 +1,14 @@
 import { test } from '@playwright/test';
+import { ManagerLoginPage } from '../../../src/pages/manager/ManagerLoginPage';
 
 test('Assert manager can Login', async ({ page }) => {
-  /* 
-  Test:
-  1. Open Wizard bank home page 
-    https://www.globalsqa.com/angularJs-protractor/BankingProject/#/login
-  2. Click [Bank Manager Login]
-  3. Assert button [Add Customer] is visible
-  4. Assert button [Open Account] is visible
-  5. Assert button [Customers] is visible
-  */
+  const loginPage = new ManagerLoginPage(page);
+
+  await loginPage.open();
+  await loginPage.clickBankManagerLogin();
+  await loginPage.assertAddCustomerButtonVisible();
+  await loginPage.assertOpenAccountButtonVisible();
+  await loginPage.assertCustomersButtonVisible();
 });
+
+
