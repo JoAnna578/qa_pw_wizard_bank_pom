a0f50c321ea736cbc6d30283a7bd442d05fc0d91
JoAnna578 <joannakoloczek1@gmail.com>
2025-12-05 18:03:22 +0100

    Update managerCanChooseCurrencies.spec.js
    Add test for selecting currencies on Open Account page using POM.

--- tests/manager/openAccount/managerCanChooseCurrencies.spec.js
+++ tests/manager/openAccount/managerCanChooseCurrencies.spec.js
@@ -1,16 +1,13 @@
 import { test } from '@playwright/test';
-import { faker } from '@faker-js/faker';
+import { ManagerOpenAccountPage } from '../../../src/pages/manager/ManagerOpenAccountPage';
 
 test('Assert manager can choose currencies for account', async ({ page }) => {
-  /* 
-  Test:
-  1. Open the Open account page 
-    https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager/openAccount
-  2. Select currency Dollar
-  3. Assert the drop-dwon has value Dollar
-  4. Select currency Pound
-  5. Assert the drop-dwon has value Pound
-  6. Select currency Rupee
-  7. Assert the drop-dwon has value Rupee
-  */
+  const openAccountPage = new ManagerOpenAccountPage(page);
+  await openAccountPage.open();
+  await openAccountPage.selectCurrency('Dollar');
+  await openAccountPage.assertSelectedCurrency('Dollar');
+  await openAccountPage.selectCurrency('Pound');
+  await openAccountPage.assertSelectedCurrency('Pound');
+  await openAccountPage.selectCurrency('Rupee');
+  await openAccountPage.assertSelectedCurrency('Rupee');
 });
