c5c688d8dc5bb906827cf8db7019da8bd1744cc5
JoAnna578 <joannakoloczek1@gmail.com>
2025-12-05 20:51:14 +0100

    Update OpenAccountPage.js
    Add ManagerOpenAccountPage POM with locators, actions, and assertions.

--- src/pages/manager/OpenAccountPage.js
+++ src/pages/manager/OpenAccountPage.js
@@ -1,13 +1,41 @@
 import { expect } from '@playwright/test';
 
-export class AddCustomerPage {
+export class ManagerOpenAccountPage {
   constructor(page) {
     this.page = page;
+
+    this.customerSelect = page.locator('#userSelect');
+    this.currencySelect = page.locator('#currency');
+    this.processButton = page.locator('button[type="submit"]');
+    this.alert = page.locator('.alert');
   }
 
   async open() {
-    await this.page.goto(
-      '/angularJs-protractor/BankingProject/#/manager/openAccount',
-    );
+    await this.page.goto('/angularJs-protractor/BankingProject/#/manager/openAccount');
+  }
+
+  async selectCustomer(name) {
+    await this.customerSelect.selectOption({ label: name });
+  }
+
+  async selectCurrency(currency) {
+    await this.currencySelect.selectOption({ label: currency });
+  }
+
+  async clickProcess() {
+    await this.processButton.click();
+  }
+
+  async assertAccountCreatedAlertVisible() {
+    await expect(this.alert).toBeVisible();
+  }
+
+  async reloadPage() {
+    await this.page.reload();
+  }
+
+  async assertSelectedCurrency(currency) {
+    await expect(this.currencySelect).toHaveValue(currency);
   }
 }
+
