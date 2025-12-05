beb4ee325e61f44d0d03bd873073fd06a7758930
JoAnna578 <joannakoloczek1@gmail.com>
2025-12-05 17:21:46 +0100

    Update managerCanAddNewCustomer.spec.js
    Add test for manager to add new customer using POM

--- tests/manager/addCustomer/managerCanAddNewCustomer.spec.js
+++ tests/manager/addCustomer/managerCanAddNewCustomer.spec.js
@@ -1,29 +1,38 @@
 import { test } from '@playwright/test';
 import { faker } from '@faker-js/faker';
+import { ManagerAddCustomerPage } from '../../../src/pages/manager/ManagerAddCustomerPage';
+import { ManagerCustomerListPage } from '../../../src/pages/manager/ManagerCustomerListPage';
 
 test('Assert manager can add new customer', async ({ page }) => {
-  /* 
-  Test:
-  1. Open add customer page by link
-    https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager/addCust
-  2. Fill the First Name.  
-  3. Fill the Last Name.
-  4. Fill the Postal Code.
-  5. Click [Add Customer].
-  6. Reload the page (This is a simplified step to close the popup)
-  7. Click [Customers] button.
-  8. Assert the customer First Name is present in the table in the last row. 
-  9. Assert the customer Last Name is present in the table in the last row. 
-  10. Assert the customer Postal Code is present in the table in the last row. 
-  11. Assert there is no account number for the new customer in the last row. 
+  // Generowanie danych testowych
+  const firstName = faker.person.firstName();
+  const lastName = faker.person.lastName();
+  const postCode = faker.location.zipCode();
 
-  Tips:
-  1. Use faker for test data generation, example usage:
-    const firstName = faker.person.firstName();
-    const lastName = faker.person.LastName();
-    const postCode = faker.location.zipCode(); 
+  // Inicjalizacja klas POM
+  const addCustomerPage = new ManagerAddCustomerPage(page);
+  const customerListPage = new ManagerCustomerListPage(page);
 
-  2. Do not rely on the customer row id for the steps 8-11. 
-    Use the ".last()" locator to get the last row.
-  */
+  // 1-5. Dodanie nowego klienta
+  await addCustomerPage.open();
+  await addCustomerPage.fillFirstName(firstName);
+  await addCustomerPage.fillLastName(lastName);
+  await addCustomerPage.fillPostCode(postCode);
+  await addCustomerPage.clickAddCustomer();
+
+  // 6. Zamknięcie popupu / przeładowanie strony
+  await addCustomerPage.reloadPage();
+
+  // 7. Przejście do listy klientów
+  await customerListPage.open();
+
+  // 8-11. Asercje dla ostatniego wiersza tabeli
+  await customerListPage.assertLastRowContains(firstName);
+  await customerListPage.assertLastRowContains(lastName);
+  await customerListPage.assertLastRowContains(postCode);
+  await customerListPage.assertLastRowNoAccountNumber();
 });
+  await customerListPage.assertLastRowNoAccountNumber();
+});
+
+
