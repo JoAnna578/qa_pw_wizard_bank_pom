6124b3d285d11827246a3afcde94de1e321c0764
JoAnna578 <joannakoloczek1@gmail.com>
2025-12-05 20:12:08 +0100

    Update BankManagerMainPage.js
    Add BankManagerMainPage POM with locators, actions, and assertions.

--- src/pages/manager/BankManagerMainPage.js
+++ src/pages/manager/BankManagerMainPage.js
@@ -3,9 +3,37 @@ import { expect } from '@playwright/test';
 export class BankManagerMainPage {
   constructor(page) {
     this.page = page;
+    this.addCustomerButton = page.locator('button[ng-click="addCust()"]');
+    this.openAccountButton = page.locator('button[ng-click="openAccount()"]');
+    this.customersButton = page.locator('button[ng-click="showCust()"]');
   }
 
   async open() {
     await this.page.goto('/angularJs-protractor/BankingProject/#/manager');
   }
+
+  async clickAddCustomer() {
+    await this.addCustomerButton.click();
+  }
+
+  async clickOpenAccount() {
+    await this.openAccountButton.click();
+  }
+
+  async clickCustomers() {
+    await this.customersButton.click();
+  }
+
+  async assertAddCustomerButtonVisible() {
+    await expect(this.addCustomerButton).toBeVisible();
+  }
+
+  async assertOpenAccountButtonVisible() {
+    await expect(this.openAccountButton).toBeVisible();
+  }
+
+  async assertCustomersButtonVisible() {
+    await expect(this.customersButton).toBeVisible();
+  }
 }
+
